46a3e4fe5ba18455b4dffdc0f990ee5ed5592d90
developer1607 <developer1607@gmail.com>
2026-06-10 14:24:21 +0530

    Working on the APIs

--- src/pages.config.js
+++ src/pages.config.js
@@ -10,29 +10,29 @@ import CallRouting from "./pages/CallRouting";
 import Contacts from "./pages/Contacts";
 import Dashboard from "./pages/Dashboard";
 import E911Reports from "./pages/E911Reports";
-import E911Review from "./pages/E911Review";
-import EndpointControl from "./pages/EndpointControl";
-import Extensions from "./pages/Extensions";
+// import E911Review from "./pages/E911Review";
+// import EndpointControl from "./pages/EndpointControl";
+// import Extensions from "./pages/Extensions";
 import KnowledgeBase from "./pages/KnowledgeBase";
 import Leads from "./pages/Leads";
-import OfflineEndpoints from "./pages/OfflineEndpoints";
+// import OfflineEndpoints from "./pages/OfflineEndpoints";
 import Opportunities from "./pages/Opportunities";
 import PBXDashboard from "./pages/PBXDashboard";
 import PBXDomains from "./pages/PBXDomains";
-import PBXPhoneNumbers from "./pages/PBXPhoneNumbers";
+// import PBXPhoneNumbers from "./pages/PBXPhoneNumbers";
 import PBXMakeCall from "./pages/PBXMakeCall";
-import PBXRouteByAni from "./pages/PBXRouteByAni";
+// import PBXRouteByAni from "./pages/PBXRouteByAni";
 import Profile from "./pages/Profile";
 import Reports from "./pages/Reports";
 import Settings from "./pages/Settings";
-import SIPALG from "./pages/SIPALG";
+// import SIPALG from "./pages/SIPALG";
 import SIPTrunks from "./pages/SIPTrunks";
-import SupportDashboard from "./pages/SupportDashboard";
-import SupportTicketDetail from "./pages/SupportTicketDetail";
-import SupportTickets from "./pages/SupportTickets";
+// import SupportDashboard from "./pages/SupportDashboard";
+// import SupportTicketDetail from "./pages/SupportTicketDetail";
+// import SupportTickets from "./pages/SupportTickets";
 import Troubleshooting from "./pages/Troubleshooting";
 import UserManagement from "./pages/UserManagement";
-import Voicemail from "./pages/Voicemail";
+// import Voicemail from "./pages/Voicemail";
 import __Layout from "./Layout.jsx";
 
 export const PAGES = {
@@ -44,29 +44,29 @@ export const PAGES = {
   Contacts,
   Dashboard,
   E911Reports,
-  E911Review,
-  EndpointControl,
-  Extensions,
+  // E911Review,
+  // EndpointControl,
+  // Extensions,
   KnowledgeBase,
   Leads,
-  OfflineEndpoints,
+  // OfflineEndpoints,
   Opportunities,
   PBXDashboard,
   PBXDomains,
-  PBXPhoneNumbers,
+  // PBXPhoneNumbers,
   PBXMakeCall,
-  PBXRouteByAni,
+  // PBXRouteByAni,
   Profile,
   Reports,
   Settings,
-  SIPALG,
+  // SIPALG,
   SIPTrunks,
-  SupportDashboard,
-  SupportTicketDetail,
-  SupportTickets,
+  // SupportDashboard,
+  // SupportTicketDetail,
+  // SupportTickets,
   Troubleshooting,
   UserManagement,
-  Voicemail,
+  // Voicemail,
 };
 
 export const pagesConfig = {
